ec09b655e9fddafef7eb036cac09a93e41f72919
Yongqi Xu <chewing_cake@icloud.com>
2021-09-01 11:51:18 -0700

    Initial Commit

---
+++ SwiftUI Gallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Gallery
+//
+//  Created by Yongqi Xu on 2021-09-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Gallery/SwiftUI_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GalleryApp.swift
+//  SwiftUI Gallery
+//
+//  Created by Yongqi Xu on 2021-09-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
